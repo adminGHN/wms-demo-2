9d5f43bfc19606befb744247aa830647b4c3bedd
adminGHN <duonghoainam35@gmail.com>
2025-07-29 23:23:01 +0700

    Add files via upload

---
+++ wms-demo (2)/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Chào mừng đến với phần mềm quản lý kho (WMS)</h1>
+    </div>
+  );
+}
